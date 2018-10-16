d039b215d83117fb5f51b56f0ba9132209dedbdf
PettoDavida <maximilian.widman@gmail.com>
2018-10-16 09:53:07 +0200

    Wow

---
+++ src/Metoder/Method6.java
@@ -0,0 +1,19 @@
+package Metoder;
+
+public class Method6 {
+
+    public static void main(String args[]){
+
+        stairs(10);
+    }
+    public static int stairs(int steps){
+        char x = 'x';
+            for (int i = 0; i < steps; i++ ){
+                for (int y = 0; y <= i; y++) {
+                    System.out.print(x);
+                }
+                System.out.println();
+            }
+            return steps;
+        }
+    }
