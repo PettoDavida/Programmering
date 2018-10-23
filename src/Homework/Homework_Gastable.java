dfb8beefdd4d286bffccc9de18df8d17447cffed
PettoDavida <maximilian.widman@gmail.com>
2018-10-23 09:26:36 +0200

    Wow

--- src/Homework/Homework_Gastable.java
+++ src/Homework/Homework_Gastable.java
@@ -5,7 +5,9 @@ public enum Homework_Gastable {
     AIR(1.29, 1.01),
     O2(1.43, 0.92),
     OXYGEN(1.43, 0.92),
-
+    HELIUM(0.18,5.1),
+    XENON(5.89,0.16),
+    METHANE(0.72,2.21)
     ;
 
     double density;
