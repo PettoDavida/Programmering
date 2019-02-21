203eeeeaeb2d98ef7fb90fb2eb581bc2eae7c5b2
PettoDavida <maximilian.widman@gmail.com>
2019-02-21 13:03:16 +0100

    Programmering

--- src/Kattis/Racing_around_the_alphabet.java
+++ src/Kattis/Racing_around_the_alphabet.java
@@ -9,16 +9,35 @@ public class Racing_around_the_alphabet {
 
         Scanner tal = new Scanner(System.in);
 
+        System.out.println();
+        int bogdan = 0;
+        int dobondi = (int) (Math.random() * 10 ) + 1;
+        String bogdish = "";
         boolean nog = false;
         boolean nig = false;
 
-        int hemligt = tal.nextInt();
+
 
         do {
             while(!nog){
-
+                bogdan = tal.nextInt();
+                if(bogdan == dobondi){
+                    System.out.println("Rätt");
+                    break;
+                }else if(bogdan > dobondi){
+                    System.out.println("Lägre"); //Här printar den lägre om talet man gissat är större än talet man ska hitta
+                }else if (bogdan < dobondi){
+                    System.out.println("Högre"); //Här printar den högre om talet man gissat är lägre än talet man ska hitta
+                }
             }
 
+            System.out.println("Vill du köra igen? Svara med ja eller nej.");
+            bogdish = tal.nextLine();
+            if (bogdish == "ja"){
+                nog = false;
+            }else if (bogdish == "nej"){
+                nig = true;
+            }
         }while(!nig);
 
     }
