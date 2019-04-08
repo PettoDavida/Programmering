7c612105c6bc1bef25f78dc39a664e28515d290b
PettoDavida <maximilian.widman@gmail.com>
2019-04-08 11:30:21 +0200

    Andra Commit. Lade in en Ascii av en hängande gubbe med en metod som tar in fel gissningar och printar ut de olika stegen tills gubben hänger.

--- C#/HangMan/HangMan/Program.cs
+++ C#/HangMan/HangMan/Program.cs
@@ -1,30 +1,140 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Threading;
 
 namespace HangMan
 {
+    class CustomWords
+    {
+        public List<string> words;
+    }
 
     class Program
     {
+
+        static string[] art = {
+@"
+
+
+
+
+
+        ",
+@"
+
+
+
+
+
+=========",
+
+@"      +
+      |
+      |
+      |
+      |
+      |
+=========",
+
+@"  +---+
+      |
+      |
+      |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========",
+
+@"  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========="
+
+};
+        static List<string> Words = new List<string> { "jesus", "owl", "peasant", "cupboard", "pizza", "buttplug", "farmer", };
+
+        static Random number = new Random();
+        static int wordindex;
+
+        static string answer;
+
+        static char[] word;
+        static List<char> correctGuess = new List<char>();
+        static List<char> wrongGuess = new List<char>();
+        static bool running;
+
         static void Main(string[] args)
         {
-            List<string> Words = new List<string> { "jesus", "owl", "peasant", "cupboard", "pizza", "buttplug", "farmer", };
 
-            Random number = new Random();
-            int wordindex = number.Next(0, Words.Count);
+            wordindex = number.Next(0, Words.Count);
 
-            string answer = Words[wordindex];
+            answer = Words[wordindex];
 
-            char[] word = answer.ToCharArray();
-            List<char> correctGuess = new List<char>();
-            List<char> wrongGuess = new List<char>();
-            bool running = true;
+            word = answer.ToCharArray();
+
+            running = true;
 
             Console.WriteLine(answer);
 
             while (running)
             {
+                if (wrongGuess.Count == art.Length - 1)
+                {
+                    return;
+                }
+
                 Console.WriteLine("Guesses");
                 foreach (char c in wrongGuess)
                 {
@@ -44,7 +154,10 @@ namespace HangMan
                     }
                 }
 
+
                 char guess = Console.ReadLine()[0];
+
+
 
                 bool added = false;
                 for (int i = 0; i < word.Length; i++)
@@ -64,11 +177,27 @@ namespace HangMan
                 {
                     if (!wrongGuess.Contains(guess))
                         wrongGuess.Add(guess);
+
                 }
+                HangManArt(wrongGuess.Count);
             }
 
+            
+        }
+        public static void HangManArt(int j)
+        {
+            if (j < art.Length)
+            {
+                Console.WriteLine(art[j]);
+            }
 
         }
+       
+       
 
-    }
-}
+           
+
+
+            }
+        }
+    
