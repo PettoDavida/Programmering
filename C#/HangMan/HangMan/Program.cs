4bf35d4adf13802a64a1bf612b03222e6cf19b6f
PettoDavida <maximilian.widman@gmail.com>
2019-04-09 13:37:46 +0200

    elfte commit. Jag förbereder koden för att man ska kunna skriva in sitt egna ord om man väljer det. Detta är så att man kan få kompisar till att gissa ett ord som du vill att de ska gissa som i hänga gubbe.

--- C#/HangMan/HangMan/Program.cs
+++ C#/HangMan/HangMan/Program.cs
@@ -129,12 +129,14 @@ namespace HangMan
             }
 
             Console.WriteLine("Do you want to play or add custom words?");
+            Console.WriteLine("You can also choose your own word if you want to play with a friend!");
             Console.WriteLine("It is recommended to add words if it is your first time!");
             Thread.Sleep(1500);
             Console.Clear();
 ForbiddenMagic4:
             Console.WriteLine("If you want to add words write (add)");
             Console.WriteLine("If you want to play write (play)");
+            Console.WriteLine("If you want to choose your own word write(own)");
             PlayorAdd = Console.ReadLine();
             Console.Clear();
             if (PlayorAdd.ToLower() == "play")
@@ -171,8 +173,12 @@ ForbiddenMagic5:
                     goto ForbiddenMagic5;
                 }
             }
-            else
+            else if (PlayorAdd.ToLower() == "own")
             {
+                goto ForbiddenMagic10;
+            }
+            else
+                    {
                 Console.WriteLine("Please try again!");
                 goto ForbiddenMagic4;
             }
@@ -181,7 +187,8 @@ ForbiddenMagic5:
 
 ForbiddenMagic:
             Game();
-
+ForbiddenMagic10:
+            OwnGame();
             if (wrongGuess.Count == art.Length - 1)
             {
                 Console.WriteLine("You Lost!");
@@ -351,6 +358,97 @@ Forbiddenmagic3:
 
             }
         }
+        public static void OwnGame()
+        {
+            wordindex = number.Next(0, Words.Count);
+
+            answer = Words[wordindex];
+
+            word = answer.ToCharArray();
+
+            running = true;
+
+            Console.WriteLine(answer);
+
+            char[] correctAnswer = new char[answer.Length];
+            for (int i = 0; i < correctAnswer.Length; i++)
+            {
+                correctAnswer[i] = '_';
+            }
+
+            while (running)
+            {
+                if (wrongGuess.Count == art.Length - 1)
+                {
+                    return;
+                }
+
+                Console.WriteLine("Guesses");
+                foreach (char c in wrongGuess)
+                {
+                    Console.Write(c + " ");
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine();
+                for (int i = 0; i < correctAnswer.Length; i++)
+                {
+                    Console.Write(correctAnswer[i] + " ");
+                }
+                Console.WriteLine();
+
+                string guess = "";
+                try
+                {
+                    guess = Console.ReadLine();
+                    Console.Clear();
+
+                }
+                catch (Exception e)
+                {
+                    goto Forbiddenmagic3;
+                }
+
+                if (guess == answer)
+                {
+                    Console.WriteLine("You got it right!");
+                    running = false;
+                    won = true;
+                }
+
+                bool added = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == guess[0])
+                    {
+                        correctAnswer[i] = word[i];
+                        if (!correctGuess.Contains(guess[0]))
+                        {
+                            correctGuess.Add(word[i]);
+                            added = true;
+                        }
+                    }
+
+                }
+
+                if (!added)
+                {
+                    if (!wrongGuess.Contains(guess[0]))
+                        wrongGuess.Add(guess[0]);
+
+                }
+
+                string word2 = new string(correctAnswer);
+
+                if (answer == word2)
+                {
+                    Console.WriteLine("You got it right!");
+                    running = false;
+                    won = true;
+                }
+
+            Forbiddenmagic3:
+                HangManArt(wrongGuess.Count);
+            }
 
     }
 }
